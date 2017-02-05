4d5c0b313fab9771d2cbc41ae1640fa7d6fc5eab
YCKAO <yunchung0529@gmail.com>
2017-02-05 19:01:27 +0800

    Estimate pi with Monte Carlo Method

---
+++ hw2/Task2/pi_free.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
+int threads=2;
+int total_number_of_points=0;
+int number_of_points_in_circle=0;
+
+
+void* pie_counter(void* arg){
+int count=0;
+ unsigned seed=(unsigned)time(NULL);
+ int times=total_number_of_points/threads;
+ while((times--)>0){
+  float x=rand_r(&seed);
+      x = x / (float)RAND_MAX ;
+  float y=rand_r(&seed);
+      y = y / (float)RAND_MAX ; 
+ 
+  if((x*x+y*y)<=1){
+   count++;
+  }
+ }
+ number_of_points_in_circle+=count;
+ pthread_exit(NULL);
+}
+
+
+int main(int argc,const char *argv[]){
+ if(argc < 1) return 0;
+ total_number_of_points=atoi(argv[1]);
+
+ pthread_t tid[threads];
+ int i;
+
+ for(i=0;i<threads;i++)
+   pthread_create(&tid[i],NULL,pie_counter,0);
+ for(i=0;i<threads;i++)
+   pthread_join(tid[i],NULL);
+
+  printf("pi estimate = %f\n",(float)4*number_of_points_in_circle/(total_number_of_points));
+return 0;
+}
---
+++ hw2/Task2/pi_lock.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
+int threads=2;
+int total_number_of_points=0;
+int number_of_points_in_circle=0;
+
+pthread_mutex_t mutex;
+
+void* pie_counter(void* arg){
+unsigned seed=(unsigned)time(NULL);
+ while(1){
+     
+   pthread_mutex_lock(&mutex);
+   if(total_number_of_points==0){
+       pthread_mutex_unlock(&mutex);
+       break;
+   }
+   total_number_of_points--;
+   pthread_mutex_unlock(&mutex);
+  
+  float x=rand_r(&seed);
+      x = x / (float)RAND_MAX ;
+  float y=rand_r(&seed);
+      y = y / (float)RAND_MAX ; 
+ 
+  if((x*x+y*y)<=1){
+   pthread_mutex_lock(&mutex);   
+   number_of_points_in_circle++;
+   pthread_mutex_unlock(&mutex);
+  }
+ } 
+ pthread_exit(NULL);
+}
+
+
+int main(int argc,const char *argv[]){
+ if(argc < 1) return 0;
+ total_number_of_points=atoi(argv[1]);
+ 
+ pthread_mutex_init(&mutex,NULL);
+ pthread_t tid[threads];
+ int i,times=total_number_of_points;
+
+ for(i=0;i<threads;i++)
+   pthread_create(&tid[i],NULL,pie_counter,0);
+ for(i=0;i<threads;i++)
+   pthread_join(tid[i],NULL);
+
+  printf("pi estimate = %f\n",(float)4*number_of_points_in_circle/times);
+return 0;
+}
